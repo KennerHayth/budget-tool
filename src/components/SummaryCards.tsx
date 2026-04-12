ae2e4ec7d2f4e6c9cfa90edbb463459c6605ec61
Kenner Hayth <kennerspc@gmail.com>
2026-04-12 16:56:15 -0400

    Create all files needed for homepage. Introduce Greeting to the home page (unformatted).

---
+++ src/components/SummaryCards.tsx
@@ -0,0 +1,17 @@
+import { type LucideIcon } from "lucide-react"
+
+
+type direction = {
+    dir: "up" | "down" | "neutral"
+}
+
+interface CardData{
+    label:string;
+    value:string;
+    sub:string;
+    trend: direction;
+    icon ?:LucideIcon;
+    iconBG ?: string;
+    iconColor ?:string;
+
+}
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,20 +1,16 @@
 import Sidebar from "../components/Sidebar"
+import Greeting from "../components/Greeting"
 
 export default function Homepage() {
     return(
-        <div style = {{display:"grid",gridTemplateColumns:"200px 1fr 1fr", background:"#272B33",overflow:"visible"}}>
+        <div style = {{display:"grid",gridTemplateColumns:"200px 1fr", background:"#272B33",overflow:"visible"}}>
             <div style = {{
             }}>
                 <Sidebar/>
             </div>
-            <div style = {{display:"grid",gridTemplateColumns:"1fr 1fr"}}>
+            <div style = {{display:"grid"}}>
                 <div>
-                    <h1>Welcome!</h1>
-                    <p>placeholder2</p>
-                </div>
-              <div>
-                    <h1>placeholder</h1>
-                    <p>placeholder2</p>
+                    <Greeting/>
                 </div>
             </div>
             <h1>test</h1>
